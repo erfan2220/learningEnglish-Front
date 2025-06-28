7d0802883c12758d086eec6ce4de27d00b9b0bd6
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-28 10:21:15 +0330

    Create mock tutor data including languages and flags

---
+++ learning/mock/tutorDetail.ts
@@ -0,0 +1,107 @@
+import videoPic from "../assets/images/videoPic.svg"
+import ukFlag from "../assets/icons/ukFlag.svg"
+import franceFlag from "../assets/icons/frenchFlag.svg"
+import irFlag from "../assets/icons/irFlag.svg"
+
+export const tutorDetail=[
+    {
+        id:1,
+        name:"Charlotte Watson",
+        role:"Professional Teacher",
+        speaks:[
+            {
+            language:"English",
+            flag:ukFlag
+        },
+
+        {
+            language:"French",
+            flag:franceFlag
+        },
+        ],
+
+        price:"USD 12.8",
+        introduceVideo:videoPic
+    },
+
+    {
+        id:2,
+        name:"Harry Potter",
+        role:"Professional Teacher",
+        speaks:[
+            {
+            language:"English",
+            flag:ukFlag
+        },
+
+        {
+            language:"Farsi",
+            flag:irFlag
+        },
+        ],
+        price:"USD 12.8",
+        introduceVideo:videoPic
+    },
+
+
+    {
+        id:3,
+        name:"Hermione Granger",
+        role:"Professional Teacher",
+        speaks:[
+            {
+            language:"English",
+            flag:ukFlag
+        },
+
+        {
+            language:"Farsi",
+            flag:irFlag
+        },
+        ],
+        price:"USD 22.5",
+        introduceVideo:videoPic
+    },
+
+    {
+        id:4,
+        name:"Draco Malfoy",
+        role:"Professional Teacher",
+        speaks:[
+            {
+            language:"English",
+            flag:ukFlag
+        },
+
+        {
+            language:"French",
+            flag:franceFlag
+        },
+        ],
+        price:"USD 30",
+        introduceVideo:videoPic
+    },
+
+    {
+        id:5,
+        name:"Luna Lovegood",
+        role:"Professional Teacher",
+        speaks:[
+            {
+            language:"English",
+            flag:ukFlag
+        },
+
+        {
+            language:"French",
+            flag:franceFlag
+        },
+        {
+            language:"Farsi",
+            flag:irFlag
+        },
+        ],
+        price:"USD 16.8",
+        introduceVideo:videoPic
+    },
+]
