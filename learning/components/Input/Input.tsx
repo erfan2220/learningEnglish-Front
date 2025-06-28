405ce074b4803d3c23267f557b0d2826a2beba6d
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-28 14:02:15 +0330

    feat: Input component created

--- learning/components/Input/Input.tsx
+++ learning/components/Input/Input.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+interface InputsProps {
+  type: string;
+  label: string;
+  value?: string | number;
+  placeholder: string;
+  onchange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  borderColor?: string;
+  focusBorderColor?: string;
+  width?: string;
+}
+
+const Inputs: React.FC<InputsProps> = ({
+  type,
+  value,
+  label,
+  placeholder,
+  onchange,
+  width = "300px",
+}) => {
+  return (
+    <div>
+      <label className="text-xs mx-2 text-[#45444A]">{label}</label>
+      <input
+        className={`border-2 border-[#D2D2D2] focus:border-[#5F33E1] rounded-2xl px-2 py-3 mb-2 mx-2 bg-white/80 text-sm h-11 focus:outline-0`}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        style={{
+          width: width,
+        }}
+        onChange={onchange}
+      />
+    </div>
+  );
 };
 
-export default Input;
+export default Inputs;
--- learning/components/SignIn/SignIn.tsx
+++ learning/components/SignIn/SignIn.tsx
@@ -2,14 +2,43 @@ import React from "react";
 import Button from "../Button/Button";
 
 import Layout from "../Layout/Layout";
+import Inputs from "../Input/Input";
+import Link from "next/link";
 
 const SignIn = () => {
   return (
-    <div className="p-6 md:p-12 max-w-[1320px] mx-auto">
+    <div className="p-6 md:p-12 max-w-2xl mx-auto ">
       <div className="mt-[60px]">
         <Layout>
-          <form>
-            <Button type="button" label={"start now"} />
+          <form className="p-8 w-full">
+            <Inputs
+              type="email"
+              placeholder="Enter your Email"
+              label="Email"
+              width="100%"
+            />
+            <Inputs
+              type="password"
+              placeholder="Enter your Password"
+              label="Password"
+              width="100%"
+            />
+            <div>Forgot Password?</div>
+            <div></div>
+
+            <Button type="button" label={"start now"} widthBtn="100%" />
+
+            <div className="text-sm text-[#45444A] mt-4">
+              By clicking Log in or Continue with, you agree to{" "}
+              <u>
+                <Link href={"/terms"}>Our Terms</Link>
+              </u>{" "}
+              of Use and{" "}
+              <u>
+                <Link href={"/privacy"}>Privacy Policy</Link>
+              </u>
+              .
+            </div>
           </form>
         </Layout>
       </div>
